c7820f9ca84aa8a89b47f61ac4a7407b4bd5816d
keroro-o <obata19760205akinobu@outlook.jp>
2020-03-28 00:55:22 +0900

    表示結果を整形して表示

--- app.js
+++ app.js
@@ -45,5 +45,9 @@ rl.on('close', () => {
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
     return pair2[1].change - pair1[1].change;
   });
-  console.log(rankingArray);
+  // map 関数：Arrayの要素それぞれを、与えられた関数を適用した内容に変換するというもの
+  const rankingStrings = rankingArray.map(([key, value]) => {
+    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率：' + value.change;
+  })
+  console.log(rankingStrings);
 });
